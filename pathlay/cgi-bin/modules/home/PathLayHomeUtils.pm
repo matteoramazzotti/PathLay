72c6f4598e53421b54beb10e559171d24d98473b
Von B <lorenzo.casbarra@gmail.com>
2024-09-18 18:52:13 +0200

    refactor: fixed indentation

--- pathlay/cgi-bin/modules/home/PathLayHomeUtils.pm
+++ pathlay/cgi-bin/modules/home/PathLayHomeUtils.pm
@@ -136,156 +136,156 @@ sub DownloadZip {
 	$zip->writeToFileHandle(*STDOUT);
 }
 sub writeExpData {
-    my %args = (
-        @_
-    );
+	my %args = (
+		@_
+	);
 
-    my $mainBase = $args{mainBase};
-    my $userHome = $args{userHome};
+	my $mainBase = $args{mainBase};
+	my $userHome = $args{userHome};
 
-    my $expId = $args{expId};
-    my $dataType = $args{dataType};
-    my $expData = $args{expData};
+	my $expId = $args{expId};
+	my $dataType = $args{dataType};
+	my $expData = $args{expData};
 
-    print STDERR "$base/pathlay_users/$userHome/$expId.$dataType\n";
+	print STDERR "$base/pathlay_users/$userHome/$expId.$dataType\n";
 
-    open(OUT, ">", "$mainBase/pathlay_users/$userHome/$expId.$dataType") or die "$mainBase/pathlay_users/$userHome/$expId.$dataType : writeExpData Failed!";
-    print OUT $expData;
-    close(OUT);
+	open(OUT, ">", "$mainBase/pathlay_users/$userHome/$expId.$dataType") or die "$mainBase/pathlay_users/$userHome/$expId.$dataType : writeExpData Failed!";
+	print OUT $expData;
+	close(OUT);
 
-    return();
+	return();
 }
 sub writeExpONT {
-    
-    my %args = (
-        @_
-    );
 
-    my $mainBase = $args{mainBase};
-    my $userHome = $args{userHome};
-    my $expId = $args{expId};
-    my $expONT = $args{expONT};
-    my $expOrganism = $args{expOrganism};
+	my %args = (
+		@_
+	);
 
-    $expONT =~ s/[\x00|\x08\x0B|\x0C\x0E|\x1F\x7F|\x9F|\x87]//g;
+	my $mainBase = $args{mainBase};
+	my $userHome = $args{userHome};
+	my $expId = $args{expId};
+	my $expONT = $args{expONT};
+	my $expOrganism = $args{expOrganism};
 
-    open(OUT,">","$mainBase/pathlay_users/$userHome/$expId.ont");
-    my @array = split(/\@/,$expONT);
+	$expONT =~ s/[\x00|\x08\x0B|\x0C\x0E|\x1F\x7F|\x9F|\x87]//g;
 
-    foreach my $ont (sort @array) {
-        #$ont =~ s/[\x00|\x08\x0B|\x0C\x0E|\x1F\x7F|\x9F|\x87]//g;
-        #$ont =~ s/%(..)/pack("C", hex($1))/eg;
-        $ont =~ s/\_$expOrganism//;
-        print OUT "$ont\n";
-    }
-    close(OUT);
-    return();
+	open(OUT,">","$mainBase/pathlay_users/$userHome/$expId.ont");
+	my @array = split(/\@/,$expONT);
+
+	foreach my $ont (sort @array) {
+		#$ont =~ s/[\x00|\x08\x0B|\x0C\x0E|\x1F\x7F|\x9F|\x87]//g;
+		#$ont =~ s/%(..)/pack("C", hex($1))/eg;
+		$ont =~ s/\_$expOrganism//;
+		print OUT "$ont\n";
+	}
+	close(OUT);
+	return();
 }
 sub writeExpConf {
 
-    my %args = (
-        @_
-    );
+	my %args = (
+		@_
+	);
 
-    my $mainBase = $args{mainBase};
-    my $userHome = $args{userHome};
-    my $expId = $args{expId};
-    my $expName = $args{expName};
-    my $expComments = $args{expComments};
-    my $expOrganism = $args{expOrganism};
-    my $form = $args{form};
+	my $mainBase = $args{mainBase};
+	my $userHome = $args{userHome};
+	my $expId = $args{expId};
+	my $expName = $args{expName};
+	my $expComments = $args{expComments};
+	my $expOrganism = $args{expOrganism};
+	my $form = $args{form};
 
-    my @dataTypes = (
-        "gene",
-        "prot",
-        "urna",
-        "meth",
-        "meta",
-        "chroma"
-    );
-    print STDERR "$base/pathlay_users/$userHome/$expId.conf\n";
-    open(OUT, ">" ,"$base/pathlay_users/$userHome/$expId.conf") or die "$base/pathlay_users/$userHome/$expId.conf"."writeExpData Failed!";
-    print OUT "expname=$expName\n";
-    print OUT "comments=$expComments\n";
-    print OUT "organism=$expOrganism\n";
+	my @dataTypes = (
+		"gene",
+		"prot",
+		"urna",
+		"meth",
+		"meta",
+		"chroma"
+	);
+	print STDERR "$base/pathlay_users/$userHome/$expId.conf\n";
+	open(OUT, ">" ,"$base/pathlay_users/$userHome/$expId.conf") or die "$base/pathlay_users/$userHome/$expId.conf"."writeExpData Failed!";
+	print OUT "expname=$expName\n";
+	print OUT "comments=$expComments\n";
+	print OUT "organism=$expOrganism\n";
 
-    foreach my $dataType (@dataTypes) {
-        print OUT $dataType."IdType=".$form{$dataType."IdType"}."\n";
-        print OUT $dataType."_id_column=".$form{$dataType."_id_column"}."\n";
-        print OUT $dataType."_dev_column=".$form{$dataType."_dev_column"}."\n";
-        print OUT $dataType."_pvalue_column=".$form{$dataType."_pvalue_column"}."\n";
-    }
-    close OUT;
+	foreach my $dataType (@dataTypes) {
+		print OUT $dataType."IdType=".$form{$dataType."IdType"}."\n";
+		print OUT $dataType."_id_column=".$form{$dataType."_id_column"}."\n";
+		print OUT $dataType."_dev_column=".$form{$dataType."_dev_column"}."\n";
+		print OUT $dataType."_pvalue_column=".$form{$dataType."_pvalue_column"}."\n";
+	}
+	close OUT;
 }
 
 sub SaveData {
 
-    my %args = (
-        @_
-    );
+	my %args = (
+		@_
+	);
 
-    my $mainBase = $args{mainBase};
-    my $userHome = $args{userHome};
-    my $expId = $args{expId};
-    my $form = %{$args{form}};
+	my $mainBase = $args{mainBase};
+	my $userHome = $args{userHome};
+	my $expId = $args{expId};
+	my $form = %{$args{form}};
 
-    my %dataTypes = (
-        'gene' => 'mrna',
-        'prot' => 'prot',
-        'urna' => 'mirna',
-        'chroma' => 'chroma',
-        'meth' => 'meth',
-        'meta' => 'meta'
-    );
+	my %dataTypes = (
+		'gene' => 'mrna',
+		'prot' => 'prot',
+		'urna' => 'mirna',
+		'chroma' => 'chroma',
+		'meth' => 'meth',
+		'meta' => 'meta'
+	);
 
-    foreach my $dataType (keys %dataTypes) {
-        writeExpData(
-            mainBase => $mainBase,
-            userHome => $userHome,
-            expId => $expId,
-            expData => $form{$dataType."_data"},
-            dataType => $dataTypes{$dataType}
-        );
-    }
-    
+	foreach my $dataType (keys %dataTypes) {
+		writeExpData(
+			mainBase => $mainBase,
+			userHome => $userHome,
+			expId => $expId,
+			expData => $form{$dataType."_data"},
+			dataType => $dataTypes{$dataType}
+		);
+	}
+
 }
 sub DeleteData {
 
-    my %args = (
-        @_
-    );
+	my %args = (
+		@_
+	);
 
-    my $mainBase = $args{mainBase};
-    my $userHome = $args{userHome};
-    my $expId = $args{expId};
+	my $mainBase = $args{mainBase};
+	my $userHome = $args{userHome};
+	my $expId = $args{expId};
 
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".conf") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".conf");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".last") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".last");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".mrna") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".mrna");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".mirna") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".mirna");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".meta") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".meta");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".meth") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".meth");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".prot") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".prot");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".chroma") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".chroma");
-    }
-    if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".sel") {
-        unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".sel");
-    }
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".conf") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".conf");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".last") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".last");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".mrna") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".mrna");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".mirna") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".mirna");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".meta") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".meta");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".meth") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".meth");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".prot") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".prot");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".chroma") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".chroma");
+	}
+	if (-e "$mainBase/pathlay_users/".$userHome."/".$expId.".sel") {
+		unlink("$mainBase/pathlay_users/".$userHome."/".$expId.".sel");
+	}
 }
 
 sub PoolDownload {
