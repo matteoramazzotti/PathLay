3e64767f42e7dd2baa5d999ac81ff4c335e632e8
Von B <lorenzo.casbarra@gmail.com>
2024-09-20 16:10:05 +0200

    feat: introduced checkClient.pl

---
+++ cgi-bin/checkClient.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use CGI;
+use CGI::Session;
+use CGI::Carp qw(fatalsToBrowser);
+use JSON;
+
+my $cgi = CGI->new;
+my $client = $ENV{'REMOTE_ADDR'};
+my $host = "127.0.0.1";
+my $response = {
+	accessGranted => $client ne $host ? "false" : "true"
+};
+$response = to_json($response);
+print $cgi->header('application/json');
+print $response;
