c51710767f4cabd4384228fd951e5c0ac9567e3b
Von B <lorenzo.casbarra@gmail.com>
2024-09-24 14:23:50 +0200

    feat: checks client by ipv6

--- cgi-bin/checkClient.pl
+++ cgi-bin/checkClient.pl
@@ -9,6 +9,9 @@ use JSON;
 my $cgi = CGI->new;
 my $client = $ENV{'REMOTE_ADDR'};
 my $host = "127.0.0.1";
+if ($client eq "::1") { #handling ipv6
+	$client = "127.0.0.1";
+}
 my $response = {
 	accessGranted => $client ne $host ? "false" : "true"
 };
