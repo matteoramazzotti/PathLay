f25a3010b0220562739fbbf679ce1dbf509d7d98
Von B. <lorenzo.casbarra@gmail.com>
2025-02-25 15:25:18 +0100

    feat: pathlay.pl now is divided between pathlayIntegrator.pl and pathlayMaps.pl

--- cgi-bin/pathlayIntegrator.pl
+++ cgi-bin/pathlayIntegrator.pl
@@ -5,6 +5,8 @@ use strict;
 use warnings;
 use Data::Dumper qw(Dumper);
 use FindBin;
+use JSON;
+use Data::Structure::Util qw( unbless );
 use lib "$FindBin::Bin/modules/";
 use lib "$FindBin::Bin/modules/structures/";
 use lib "$FindBin::Bin/modules/results/";
@@ -22,49 +24,38 @@ use PathLayExpPackages;
 use PathLayComplex;
 use PathLayPathway;
 
+
+
 my $t0 = Benchmark->new;
 
 our $timestamp = getTimeStamp();
 ### defaults ###
 my $server="localserver";
 my $base = "$FindBin::Bin/..";
-our $cgi = CGI->new;
-my $session_id = $cgi->param('sid');
-my $session = CGI::Session->new($session_id);
+my $cgi = CGI->new;
+# my $session_id = $cgi->param('sid');
+
+my $json_text = $cgi->param('POSTDATA');
+
+my $json_data = decode_json($json_text);
+
+my $session_id = $json_data->{sid};
+my $session = CGI::Session->load($session_id);
 my $username = $session->param('username');
 my $home = $session->param('home');
 my $expId = $cgi->param('exp');
-my $last = $cgi->param('conf') ? $cgi->param('conf') : undef;
-
-print STDERR $home;
-print STDERR "SERVER: $base\n";
-print STDERR "SERVER: $server\n";
-my $debug = 0;
-my $runstart = time();
-my $report = new Report();
-
-
-
+print STDERR "$0: $json_data->{sid}\n";
+print STDERR "$0: $session_id\n";
+print STDERR "$0: $username\n";
+print STDERR "$0: $home\n";
 
 my $parameters = new Parameters();
+$parameters -> {_exp_name_input_text} = $expId;
 $parameters->{_userdir} = $home ne "6135251850" ? "$base/pathlay_users/".$home."/" : "$base/demo_exps/6135251850/";
 
-if (!$last) {
-  $parameters -> LoadENVFromCGI();
-} else {
-  $parameters -> LoadLastENV(
-    expId => $expId
-  );
-}
-
-if (!$parameters->{_h3}) {
-  $parameters->{_h3} = $home;
-}
-
-# $parameters -> updateLastSession();
-$parameters -> PrintParameters();
-
-
+$parameters -> LoadENVFromJson(
+	JSON => $json_data
+);
 my @dataTypes = (
   "gene",
   "prot",
@@ -151,6 +142,7 @@ if ($statOn == 0) {
   $parameters -> {_statistic_select} = "Nothing";
 }
 
+
 #db loading for ID Conversions
 @$DBs{"gene","meth","chroma"} = (
   new geneDB (
@@ -204,6 +196,7 @@ foreach my $dataType (@dataTypes) {
 
   }
 }
+
 
 $DBs -> {urna} -> DBLoader(
   ExpuRNAs => $expPackages -> {urna}
@@ -275,7 +268,7 @@ $expPackages -> {ont} -> ONTsLoader(
 );
 $interfaces -> {ont} -> integrateAll(
   DEGs => $expPackages -> {gene},
-  #NODEGs => $expPackages -> {nodeg},
+  NODEGs => $expPackages -> {nodeg},
   Prots => $expPackages -> {prot},
   ONTs => $expPackages -> {ont}
 );
@@ -398,133 +391,25 @@ my %nums_for_selector;
 
 print STDERR  "Maps to be processed: ".(scalar @maps)."\n";
 
-foreach my $map (sort(@maps)) {
-  # next if ($map !~ "hsa00020"); #hsa04933
-  my ($map_name,$mapin) = split(/_/,$map);
-  my $mapinfile = $mapin;
-  $mapinfile .= ".nodes";
-  my $organism = $parameters -> {_org};
-  my $image_file = $mapin;
-  $image_file =~ s/$organism//;
-  $image_file .= ".png";
-  my $pathway = new Pathway(
-    _source => $parameters -> {_nodesdir}."$mapinfile",
-    _index => $map_counter
-  );
-  $pathway -> PathwayLoader(
-    Params => $parameters,
-    ExpGenes  => $expPackages -> {gene},
-    ExpNoDEGs => $expPackages -> {nodeg},
-    ExpProts => $expPackages -> {prot},
-    ExpuRNAS  => $expPackages -> {urna},
-    ExpMetas  => $expPackages -> {meta}
-  );
-  #here proteins should be merged with genes
-  #print STDERR Dumper $pathway;
-  $pathway -> NodesInit();
-
-  $pathway -> LoadComplexes(
-    Parameters => $parameters
-  );
-  # print STDERR Dumper  $pathway;
-  next if (
-    scalar  @{$pathway -> {_complexes} -> {deg}} < 1 &&
-    scalar  @{$pathway -> {_complexes} -> {nodeg}} < 1 &&
-    scalar  @{$pathway -> {_complexes} -> {meta}} < 1 &&
-    scalar @{$pathway -> {_complexes} -> {prot}} < 1 &&
-    scalar @{$pathway -> {_complexes} -> {multi}} < 1
-  );
-  $pathway -> PathwayEncoder();
-  my $map_div = new MapDiv(
-    _id => $mapin,
-    _name => $map_name,
-    _class => "pathway_div  animate-right",
-    #_style => "position:absolute;z-index:1;visibility:hidden"
-    _style => "display:none;"
-  );
-  my $map_complexes_div = new MapDiv(
-    _id => $mapin."_complexes",
-    _class => "complexes_div",
-    #_style => "z-index:3;position:absolute;top:0;left:0;width:100%;height:100%;"
-    _style => "position:absolute;top:0;left:0;bottom:0;right:0;display:block;width:100%;height:100%"
-  );
-
-  my $map_img = new HTMLImg(
-    _id => $mapin."_path",
-    _src => $parameters -> {_mapdir}.$image_file,
-    _class => "animate-right",
-    _usemap => "#".$mapin."_map",
-    _border => 0
-    # _style => "position:relative;"
-  );
-  $map_div -> ContentLoader(
-    Content => $map_img
-  );
-
-  $map_complexes_div -> LoadComplexesOnDiv(
-    Pathway => $pathway
-  );
-
-  $map_div -> ContentLoader(
-    Content => $map_complexes_div
-  );
-  $map_counter++;
-  push(@map_divs,$map_div);
-  push(@map_ids,$mapin);
-  push(@map_names,$map_name);
-  #$pathway -> PrintComplexes();
-  %{$nums_for_selector{$mapin}} = (
-    degs => $pathway -> {_degs_loaded},
-    nodegs => $pathway -> {_nodegs_loaded},
-    proteins => $pathway -> {_proteins_loaded},
-    urnas => $pathway -> {_urnas_loaded},
-    metas => $pathway -> {_metas_loaded},
-    methyls => $pathway -> {_methyls_loaded}
-  );
-}
 
 
-my ($ont2gene_js_var,@ont_select_options) = prepareONTsForFrontend(
-  ONTs => $expPackages -> {ont}
-  #ONT2GeneJSVar => $ont2gene_js_var
-);
-#print STDERR Dumper $ont2gene_js_var;
-
-$map_divs[0] -> {_style} = "display:block;";
-
-my ($style_section,$script_section,$container5,$container2,$container3,$container6,$window_engine) = ResultsBuilder(
-  Parameters => $parameters,
-  MapDivs => \@map_divs,
-  MapIDs => \@map_ids,
-  MapNames => \@map_names,
-  Nums => \%nums_for_selector,
-  ont_select_options => \@ont_select_options,
-  ont2gene_js_var => $ont2gene_js_var
-);
-
- 
-
-prepareEnablersForFrontEnd(
-  Parameters => $parameters,
-  ScriptSection => $script_section
-);
-
-my $wrapper = wrapperBuilder(
-  Container5 => $container5,
-  Container2 => $container2,
-  Container3 => $container3,
-  Container6 => $container6
-);
+my $response = {
+	status => "Success",
+	messagge => "Enjoy your data",
+	data => {
+    maps => [@maps],
+    parameters => $parameters,
+    expPackages => $expPackages
+  }
+};
+$response = encode_json(unbless($response));
+$response = to_json($response);
+# print STDERR Dumper $json_data;
+print $cgi->header('application/json');
+print $response;
 
 
-printPage(
-  StyleSection => $style_section,
-  ScriptSection => $script_section,
-  Wrapper => $wrapper,
-  WindowEngine => $window_engine,
-  Parameters => $parameters
-);
-my $t1 = Benchmark->new;
-my $td = timediff($t1, $t0);
-print STDERR "DIFF:".timestr($td)."\n";
+
+exit;
+
 
---
+++ cgi-bin/pathlayMaps.pl
@@ -0,0 +1,186 @@
+#!/usr/bin/perl
+use CGI;
+use CGI::Session;
+use strict;
+use warnings;
+use Data::Dumper qw(Dumper);
+use FindBin;
+use JSON;
+use Data::Structure::Util qw( unbless );
+use lib "$FindBin::Bin/modules/";
+use lib "$FindBin::Bin/modules/structures/";
+use lib "$FindBin::Bin/modules/results/";
+use lib "$FindBin::Bin/modules/frontend/";
+use lib "$FindBin::Bin/modules/frontend/results/";
+
+use PathLayUtils;
+use PathLayIntegration;
+use PathLayResultsFrontEnd;
+use HTMLObjects;
+use Benchmark;
+use PathLayStatistic;
+use PathLayDBs;
+use PathLayExpPackages;
+use PathLayComplex;
+use PathLayPathway;
+
+
+my $t0 = Benchmark->new;
+
+our $timestamp = getTimeStamp();
+### defaults ###
+my $server="localserver";
+my $base = "$FindBin::Bin/..";
+my $cgi = CGI->new;
+my $json_text = $cgi->param('json_data');
+my $json_data = decode_json($json_text);
+
+
+my @maps = @{$json_data->{maps}};
+my $parameters = $json_data->{parameters};
+my $expPackages = $json_data->{expPackages};
+
+my @map_divs;
+my @map_ids;
+my @map_names;
+
+my $map_counter = 0;
+my %nums_for_selector;
+
+
+foreach my $map (sort(@maps)) {
+  # next if ($map !~ "hsa00020"); #hsa04933
+  my ($map_name,$mapin) = split(/_/,$map);
+  my $mapinfile = $mapin;
+  $mapinfile .= ".nodes";
+  my $organism = $parameters -> {_org};
+  my $image_file = $mapin;
+  $image_file =~ s/$organism//;
+  $image_file .= ".png";
+  my $pathway = new Pathway(
+    _source => $parameters -> {_nodesdir}."$mapinfile",
+    _index => $map_counter
+  );
+  $pathway -> PathwayLoader(
+    Params => $parameters,
+    ExpGenes  => $expPackages -> {gene},
+    ExpNoDEGs => $expPackages -> {nodeg},
+    ExpProts => $expPackages -> {prot},
+    ExpuRNAS  => $expPackages -> {urna},
+    ExpMetas  => $expPackages -> {meta}
+  );
+  #here proteins should be merged with genes
+  #print STDERR Dumper $pathway;
+  $pathway -> NodesInit();
+
+  $pathway -> LoadComplexes(
+    Parameters => $parameters
+  );
+  # print STDERR Dumper  $pathway;
+  next if (
+    scalar  @{$pathway -> {_complexes} -> {deg}} < 1 &&
+    scalar  @{$pathway -> {_complexes} -> {nodeg}} < 1 &&
+    scalar  @{$pathway -> {_complexes} -> {meta}} < 1 &&
+    scalar @{$pathway -> {_complexes} -> {prot}} < 1 &&
+    scalar @{$pathway -> {_complexes} -> {multi}} < 1
+  );
+  $pathway -> PathwayEncoder();
+  my $map_div = new MapDiv(
+    _id => $mapin,
+    _name => $map_name,
+    _class => "pathway_div  animate-right",
+    #_style => "position:absolute;z-index:1;visibility:hidden"
+    _style => "display:none;"
+  );
+  my $map_complexes_div = new MapDiv(
+    _id => $mapin."_complexes",
+    _class => "complexes_div",
+    #_style => "z-index:3;position:absolute;top:0;left:0;width:100%;height:100%;"
+    _style => "position:absolute;top:0;left:0;bottom:0;right:0;display:block;width:100%;height:100%"
+  );
+
+  my $map_img = new HTMLImg(
+    _id => $mapin."_path",
+    _src => $parameters -> {_mapdir}.$image_file,
+    _class => "animate-right",
+    _usemap => "#".$mapin."_map",
+    _border => 0
+    # _style => "position:relative;"
+  );
+  $map_div -> ContentLoader(
+    Content => $map_img
+  );
+
+  $map_complexes_div -> LoadComplexesOnDiv(
+    Pathway => $pathway
+  );
+
+  $map_div -> ContentLoader(
+    Content => $map_complexes_div
+  );
+  $map_counter++;
+  push(@map_divs,$map_div);
+  push(@map_ids,$mapin);
+  push(@map_names,$map_name);
+  #$pathway -> PrintComplexes();
+  %{$nums_for_selector{$mapin}} = (
+    degs => $pathway -> {_degs_loaded},
+    nodegs => $pathway -> {_nodegs_loaded},
+    proteins => $pathway -> {_proteins_loaded},
+    urnas => $pathway -> {_urnas_loaded},
+    metas => $pathway -> {_metas_loaded},
+    methyls => $pathway -> {_methyls_loaded}
+  );
+}
+$map_divs[0] -> {_style} = "display:block;";
+
+
+
+my ($ont2gene_js_var,@ont_select_options) = prepareONTsForFrontend(
+  ONTs => $expPackages -> {ont}
+  #ONT2GeneJSVar => $ont2gene_js_var
+);
+#print STDERR Dumper $ont2gene_js_var;
+
+my ($style_section,$script_section,$container5,$container2,$container3,$container6,$window_engine) = ResultsBuilder(
+  Parameters => $parameters,
+  MapDivs => \@map_divs,
+  MapIDs => \@map_ids,
+  MapNames => \@map_names,
+  Nums => \%nums_for_selector,
+  ont_select_options => \@ont_select_options,
+  ont2gene_js_var => $ont2gene_js_var
+);
+
+ 
+
+prepareEnablersForFrontEnd(
+  Parameters => $parameters,
+  ScriptSection => $script_section
+);
+
+my $wrapper = wrapperBuilder(
+  Container5 => $container5,
+  Container2 => $container2,
+  Container3 => $container3,
+  Container6 => $container6
+);
+
+
+printPage(
+  StyleSection => $style_section,
+  ScriptSection => $script_section,
+  Wrapper => $wrapper,
+  WindowEngine => $window_engine,
+  Parameters => $parameters
+);
+my $t1 = Benchmark->new;
+my $td = timediff($t1, $t0);
+print STDERR "DIFF:".timestr($td)."\n";
+
+
+
+
+
+
+
