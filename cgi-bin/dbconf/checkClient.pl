fb8dca41af97d61364d90639990af94d9455bd6e
Von B. <lorenzo.casbarra@gmail.com>
2025-06-12 16:19:51 +0200

    fix: ipcheck

--- cgi-bin/dbconf/checkClient.pl
+++ cgi-bin/dbconf/checkClient.pl
@@ -9,7 +9,7 @@ use JSON;
 my $cgi = CGI->new;
 my $client = $ENV{'REMOTE_ADDR'};
 my $host = "127.0.0.1";
-if ($client eq "::1") { #handling ipv6
+if ($client eq "::1" || $client eq '172.17.0.1' || $client eq '192.168.0.1') { #handling ipv6 and docker
 	$client = "127.0.0.1";
 }
 my $response = {
